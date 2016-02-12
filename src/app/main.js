55f1aceb7f37182852ad87d06ad12dcbf4b82e97
Matteo Scandolo <teo@onlab.us>
2016-02-12 13:31:01 -0800

    Exercise done

--- src/app/main.js
+++ src/app/main.js
@@ -1,14 +1,58 @@
+'use strict';
+
 var data = [3, 7, 21, 31, 35, 42];
 
-const handleP = () => {
-  d3.select(".chart").selectAll("div")
-  .data(data)
-  .enter().append("div")
+var duration = 3000;
+
+const update = (data) => {
+
+  let el = d3.select(".chart")
+  .selectAll("div")
+  .data(data);
+
+  el.enter()
+  .append("div")
   .attr({
     class: 'bar'
   })
-  .style('width', d => `${d}%`)
+  .style({
+    width: '0px'
+  });
+
+  el
+  .exit()
+  .transition()
+  .duration(duration)
+  .style({
+    width: '0px'
+  })
+  .remove();
+  
+  el
+  .transition()
+  .duration(duration)
+  .style({
+    width: d => `${d * 10}px`
+  })
   .text(d => `${d}%`);
 }
 
-handleP();
+
+update(data);
+
+$(document).ready(() => {
+  $('#addBtn').click(() => {
+    data.push(Math.round(Math.random() * 50));
+    update(data);
+  });
+
+  $('#rmBtn').click(() => {
+    data.splice(data.length - 1, 1);
+    update(data);
+  });
+
+  $('#rvBtn').click(() => {
+    data = data.reverse();
+    update(data);
+  });
+})
